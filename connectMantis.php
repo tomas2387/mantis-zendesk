df7b6de3e77535bcbf1dd6f9568a1df7abfa0a3f
Tomas <tomas2387@gmail.com>
2012-08-01 15:45:03 +0200

    added the nusoap to connect to mantis

--- connectMantis.php
+++ connectMantis.php
@@ -3,33 +3,36 @@ if( !isset($_GET['projectname']) ) {
 	die('No project name!');
 }
 
-
 $pn = $_GET['projectname'];
 
-include_once('settings.php');
-require_once(NUSOAP);
+include('settings.php');
+include(NUSOAP);
+
 $soapclient = new nusoap_client(ENDPOINT);
+
+
+$projectID = $soapclient->call( 'mc_project_get_id_from_name',
+									array(
+                                        'username' => MANTIS_USERNAME,
+                                        'password' => MANTIS_PASSWORD,
+                                        'project_name' => $pn
+                                    ) );
+
+if(!isset($projectID) || empty($projectID)) {
+    die(json_encode( array('error' => "That project doesn't exists!") ));
+}
+
+$result = $soapclient->call( 'mc_project_get_issues',
+							array(
+                                'username' => 'administrator',
+                                'password' => 'root',
+                                'project_id' => $projectID,
+                                'page_number' => '',
+                                'per_page' => ''
+                            ) );
+
 
 $version = $soapclient->call( 'mc_version', array() );
 
-$projectID = $soapclient->call( 'mc_project_get_id_from_name', 
-									array(
-										'username' => MANTIS_USERNAME,
-										'password' => MANTIS_PASSWORD,
-										'project_name' => $pn
-										) );
-
-if(!isset($projectID) || empty($projectID)) {
-	die(json_encode( array('error' => "That project doesn't exists!") ));
-}
-
-$result = $soapclient->call( 'mc_project_get_issues', 
-							array(
-								'username' => 'administrator',
-								'password' => 'root',
-								'project_id' => $projectID,
-								'page_number' => '',
-								'per_page' => ''
-								) );
 echo json_encode(array('result' => $result, 'version' => $version) );
 			
--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
 define('ENDPOINT', 'http://localhost/mantis/mantisbt/api/soap/mantisconnect.php');
-define('NUSOAP', '../mantisbt/library/nusoap/nusoap.php');
+define('NUSOAP', 'library/nusoap/nusoap.php');
 
 /* Mantis credentials */
 define('MANTIS_USERNAME', 'administrator');
