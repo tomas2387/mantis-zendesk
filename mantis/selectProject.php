a8217eb235e67608a25eb94fb82eb6271ac1f3c5
Tomas Prado <tomas.prado@eyeos.com>
2012-09-13 11:51:00 +0200

    Deleted unused files and modified models and the buglist test

--- mantis/selectProject.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once(MANTIS2ZENDESK_ROOT.'/mantis/controller/projectController.php');
-
-$uc = new projectController();
-$arrayMantisProjects = $uc->getMantisProjects();
-
-include(MANTIS2ZENDESK_ROOT.'/resources/header.html');
-include(MANTIS2ZENDESK_ROOT.'/mantis/views/selectProject.php');
-include(MANTIS2ZENDESK_ROOT.'/resources/footer.html');
