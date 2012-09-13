77e96e992b6e1741c47fb5d5226ad5c1b49d8f2e
eyeos <eyeos@PC010.(none)>
2012-09-13 11:00:46 +0200

    added new test & new class item

---
+++ mantis/model/Result.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: eyeos
+ * Date: 13/09/12
+ * Time: 10:09
+ * To change this template use File | Settings | File Templates.
+ */
+class Result
+{
+    public $id;
+    public $text;
+
+}
---
+++ mantis/views/Item.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: eyeos
+ * Date: 13/09/12
+ * Time: 10:34
+ * To change this template use File | Settings | File Templates.
+ */
+abstract class Item
+{
+    abstract function renderView();
+}
