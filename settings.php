66133f5136cb852f77dd1a24b01b205762cd53e5
tomas2387 <tomas2387@gmail.com>
2012-09-21 13:11:00 +0200

    settings uploaded

---
+++ settings.php
@@ -0,0 +1,16 @@
+<?php
+/* Mantis credentials */
+define('ENDPOINT', 'http://localhost/mantisbt/api/soap/mantisconnect.php');
+define('MANTIS_USERNAME', 'administrator');
+define('MANTIS_PASSWORD', 'root');
+
+
+/* Zendesk credentials */
+
+
+define("ZDAPIKEY", "Yourzendeskapikey");
+define("ZDUSER", "your@emailatzendesk.org");
+define("ZDURL", "https://yourorganizationurl.zendesk.com/api/v2");
+
+
+/* Note: do not put a trailing slash at the end of v2 */
