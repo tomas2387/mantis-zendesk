a6707243384ef79701d45f5772d9fb6d2f66dc93
tomas2387 <tomas2387@gmail.com>
2012-10-16 10:32:08 +0200

    settings update

--- settings.php
+++ settings.php
@@ -1,16 +1,16 @@
 <?php
 /* Mantis credentials */
-define('ENDPOINT', 'http://icub.eyeos.org/mantis/api/soap/mantisconnect.php');
-define('MANTIS_USERNAME', 'tomas.prado');
-define('MANTIS_PASSWORD', 'tomastomas');
+define('ENDPOINT', 'http://MANTIS URL/mantis/api/soap/mantisconnect.php');
+define('MANTIS_USERNAME', 'MANTIS USERNAME');
+define('MANTIS_PASSWORD', 'MANTIS PASSWORD');
 
 
 /* Zendesk credentials */
 
 
-define("ZDAPIKEY", "QpGyAbg9eupGZo1Q5bChYzDktnYc88efSTBNhbQT");
-define("ZDUSER", "tomas.prado@eyeos.org");
-define("ZDURL", "https://eyeos.zendesk.com/api/v2");
+define("ZDAPIKEY", "YOUR ZENDESK API KEY");
+define("ZDUSER", "ZENDESK USER");
+define("ZDURL", "https://COMPANY.zendesk.com/api/v2");
 
 
 /* Note: do not put a trailing slash at the end of v2 */
