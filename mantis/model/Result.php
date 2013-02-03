34f9b0662b92a70ad879b4bf5190bc8f585b6a47
Tomás <tomas2387@gmail.com>
2013-02-03 12:11:38 +0100

    Update mantis/model/Result.php

--- mantis/model/Result.php
+++ mantis/model/Result.php
@@ -1,10 +1,8 @@
 <?php
 /**
  * Created by JetBrains PhpStorm.
- * User: eyeos
  * Date: 13/09/12
  * Time: 10:09
- * To change this template use File | Settings | File Templates.
  */
 class Result extends Item
 {
